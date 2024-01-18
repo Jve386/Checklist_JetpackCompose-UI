f07a41e2896fa30425beb984266a5e50e8f561e4
Jve386 <jve.formacio@gmail.com>
2024-01-18 01:37:27 +0100

    MainActivity

--- app/src/main/java/com/jve386/jetpackcomposeui/MainActivity.kt
+++ app/src/main/java/com/jve386/jetpackcomposeui/MainActivity.kt
@@ -3,13 +3,22 @@ package com.jve386.jetpackcomposeui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jve386.jetpackcomposeui.ui.theme.JetpackComposeUITheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +40,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+// YT VIDEO: 16:28 https://www.youtube.com/watch?v=6_wK_Ud8--0&ab_channel=PhilippLackner
+    ) {
+        Text(
+            text = "Hello $name!",
+            color = Color.Blue,
+            fontSize = 30.sp,
+        )
+        Text(
+            text = "Some other text",
+            color = Color.Blue,
+            fontSize = 30.sp,
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
